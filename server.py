feb509680a015689367b6221bab1bfe644607fb3
Data-Men <nee271raj@protonmail.com>
2024-03-03 21:19:29 +0530

    fix : cors

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import subprocess
 import uuid
 import os
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/execute', methods=['POST'])
 def execute_command():
